236c868d3c3b359593b3e56d1f88659a6cae9bee
Zhang <1924391701.qq.com>
2020-05-04 23:17:14 +0800

    First to use git with github

---
+++ 2020-05-04.py
@@ -0,0 +1,21 @@
+# print("hahahh", 23333)
+# print("哈"*10)
+# print(1>2)
+
+
+# a = input("请输入你要输入的整数：")
+# print("你输入的值为：" + a)
+
+
+# a1 = float(input("请输入a1："))
+# a2 = float(input("请输入a2："))
+
+# print("你输入的值为：" ,a1+a2)
+
+# a = '2o8y8whcwuig ciufgvio giv'
+# print("a的字符串长度为：", len(a))
+
+"""通过代码获取两段内容，并且计算他们的长度和"""
+a1 = input("请输入a1：")
+a2 = input("请输入a2：")
+print("两段内容的长度和为：", len(a1)+len(a2))
